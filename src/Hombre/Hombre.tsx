4519e3bbb78347e7b78b6631fc7a5c92d8b41c27
DiegoGuayara <diegofguayara9707@gmail.com>
2025-09-01 11:17:13 -0500

    Modificacion del componente hombre

--- src/Hombre/Hombre.tsx
+++ src/Hombre/Hombre.tsx
@@ -1,122 +1,28 @@
-import camiseta1 from "../assets/product-page-01-related-product-01.jpg";
-import camiseta2 from "../assets/product-page-01-related-product-02.jpg";
-import camiseta3 from "../assets/product-page-01-related-product-03.jpg";
-import camiseta4 from "../assets/product-page-01-related-product-04.jpg";
-import camisa1 from "../assets/CME_1110R_V1_A_SML.jpg";
-import camisa2 from "../assets/descarga (11).jpeg";
-import camisa3 from "../assets/descarga (12).jpeg";
-import camisa4 from "../assets/descarga (13).jpeg";
-import camibuso1 from "../assets/090200delbeige.jpg";
-import camibuso2 from "../assets/090208DELNEGRO.jpg";
-import camibuso3 from "../assets/camibuso-tipo-polo-h.jpg";
-import camibuso4 from "../assets/images (1).jpeg";
+import { imagenes } from "./imagenesPrendasHombre";
 
 export const Hombre = () => {
-  const imagenes = [
-    {
-      id: 1,
-      imagen: camiseta1,
-      titulo: "Camiseta simple",
-      descripcion: "Camiseta negra",
-      precio: "$20.000",
-    },
-    {
-      id: 2,
-      imagen: camiseta2,
-      titulo: "Camiseta simple",
-      descripcion: "Camiseta blanca",
-      precio: "$20.000",
-    },
-    {
-      id: 3,
-      imagen: camiseta3,
-      titulo: "Camiseta simple",
-      descripcion: "Camiseta gris",
-      precio: "$20.000",
-    },
-    {
-      id: 4,
-      imagen: camiseta4,
-      titulo: "Camiseta con estampado",
-      descripcion: "Camiseta rosa claro con puntos blancos, morados y cafes",
-      precio: "$20.000",
-    },
-    {
-      id: 5,
-      imagen: camisa1,
-      titulo: "Camisa simple",
-      descripcion: "Camisa verde claro manga corta",
-      precio: "$20.000",
-    },
-    {
-      id: 6,
-      imagen: camisa2,
-      titulo: "Camisa simple",
-      descripcion: "Camisa vino tinto manga larga",
-      precio: "$20.000",
-    },
-    {
-      id: 7,
-      imagen: camisa3,
-      titulo: "Camisa con estampado",
-      descripcion: "Camisa blanca manga larga con puño azul con puntos blancos",
-      precio: "$20.000",
-    },
-    {
-      id: 8,
-      imagen: camisa4,
-      titulo: "Camisa simple",
-      descripcion: "Camisa gris manga larga",
-      precio: "$20.000",
-    },
-    {
-      id: 9,
-      imagen: camibuso1,
-      titulo: "Camibuso simple",
-      descripcion: "Camibuso beige claro con mangas con lineas negras",
-      precio: "$20.000",
-    },
-    {
-      id: 10,
-      imagen: camibuso2,
-      titulo: "Camibuso simple",
-      descripcion: "Camibuso negro con botones beige y rojo",
-      precio: "$20.000",
-    },
-    {
-      id: 11,
-      imagen: camibuso3,
-      titulo: "Camibuso simple",
-      descripcion: "Camibuso gris",
-      precio: "$20.000",
-    },
-    {
-      id: 12,
-      imagen: camibuso4,
-      titulo: "Camibuso simple",
-      descripcion: "Camibuso amarillo claro",
-      precio: "$20.000",
-    },
-  ];
-
   return (
     <div className=" h-171 bg-[#f2f9f7] ">
       <h1 className="font-mono text-[30px] text-center">Ropa de hombre</h1>
 
-      <div className="flex flex-wrap justify-center mt-6 bg-[#f2f9f7] ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6 gap-6 bg-[#f2f9f7]">
         {imagenes.map((img) => (
-          <div key={img.id} className="flex m-4">
+          <div key={img.id} className="flex m-4 justify-center">
             <div className="flex flex-col w-60">
-              <img
-                src={img.imagen}
-                alt={`Camisa ${img.id}`}
-                className="w-60 h-80 object-cover rounded-lg shadow-md"
-              />
-              <div className="flex justify-between">
-                <p>{img.titulo}</p>
-                <p>{img.precio}</p>
+              <div className="overflow-hidden rounded-lg shadow-md">
+                <img
+                  src={img.imagen}
+                  alt={`Camisa ${img.id}`}
+                  className="w-60 h-80 object-cover rounded-lg shadow-md transform duration-300 hover:scale-110"
+                />
               </div>
-              <p className="text-sm text-gray-600 break-words">{img.descripcion}</p>
+              <div className="flex justify-between">
+                <p className="text-gray-800">{img.titulo}</p>
+                <p className="text-green-800">{img.precio}</p>
+              </div>
+              <p className="text-sm text-gray-500 break-words">
+                {img.descripcion}
+              </p>
             </div>
           </div>
         ))}
---
+++ src/Hombre/imagenesPrendasHombre.ts
@@ -0,0 +1,207 @@
+import camiseta1 from "../assets/camiseta1.jpg";
+import camiseta2 from "../assets/camiseta2.jpg";
+import camiseta3 from "../assets/camiseta3.jpg";
+import camiseta4 from "../assets/camiseta4.jpg";
+import camisa1 from "../assets/camisa1.jpg";
+import camisa2 from "../assets/camisa2.jpeg";
+import camisa3 from "../assets/camisa3.jpeg";
+import camisa4 from "../assets/camisa4.jpeg";
+import camibuso1 from "../assets/camibuso1.jpg";
+import camibuso2 from "../assets/camibuso2.jpg";
+import camibuso3 from "../assets/camibuso3.jpg";
+import camibuso4 from "../assets/camibuso4.jpeg";
+import saco1 from "../assets/saco1.jpeg";
+import saco2 from "../assets/saco2.jpeg";
+import saco3 from "../assets/saco3.jpeg";
+import saco4 from "../assets/saco4.jpeg";
+import pantalon1 from "../assets/jean1.jpeg";
+import pantalon2 from "../assets/jean2.jpeg";
+import pantalon3 from "../assets/jean3.jpeg";
+import pantalon4 from "../assets/jean4.jpg";
+import sudadera1 from "../assets/sudadera1.jpeg";
+import sudadera2 from "../assets/sudadera2.jpeg";
+import sudadera3 from "../assets/sudadera3.jpeg";
+import sudadera4 from "../assets/sudadera4.jpeg";
+
+export const imagenes = [
+  //Camisetas
+  {
+    id: 1,
+    imagen: camiseta1,
+    titulo: "Camiseta simple",
+    descripcion: "Camiseta negra",
+    precio: "$20.000",
+  },
+  {
+    id: 2,
+    imagen: camiseta2,
+    titulo: "Camiseta simple",
+    descripcion: "Camiseta blanca",
+    precio: "$20.000",
+  },
+  {
+    id: 3,
+    imagen: camiseta3,
+    titulo: "Camiseta simple",
+    descripcion: "Camiseta gris",
+    precio: "$20.000",
+  },
+  {
+    id: 4,
+    imagen: camiseta4,
+    titulo: "Camiseta simple",
+    descripcion: "Camiseta azul",
+    precio: "$20.000",
+  },
+
+  //Camisas
+  {
+    id: 5,
+    imagen: camisa1,
+    titulo: "Camisa simple",
+    descripcion: "Camisa verde claro manga corta",
+    precio: "$20.000",
+  },
+  {
+    id: 6,
+    imagen: camisa2,
+    titulo: "Camisa simple",
+    descripcion: "Camisa vino tinto manga larga",
+    precio: "$20.000",
+  },
+  {
+    id: 7,
+    imagen: camisa3,
+    titulo: "Camisa con estampado",
+    descripcion: "Camisa blanca manga larga con puño azul con puntos blancos",
+    precio: "$20.000",
+  },
+  {
+    id: 8,
+    imagen: camisa4,
+    titulo: "Camisa simple",
+    descripcion: "Camisa gris manga larga",
+    precio: "$20.000",
+  },
+
+  //Camibusos
+  {
+    id: 9,
+    imagen: camibuso1,
+    titulo: "Camibuso simple",
+    descripcion: "Camibuso beige claro con mangas con lineas negras",
+    precio: "$20.000",
+  },
+  {
+    id: 10,
+    imagen: camibuso2,
+    titulo: "Camibuso simple",
+    descripcion: "Camibuso negro con botones beige y rojo",
+    precio: "$20.000",
+  },
+  {
+    id: 11,
+    imagen: camibuso3,
+    titulo: "Camibuso simple",
+    descripcion: "Camibuso gris",
+    precio: "$20.000",
+  },
+  {
+    id: 12,
+    imagen: camibuso4,
+    titulo: "Camibuso simple",
+    descripcion: "Camibuso amarillo claro",
+    precio: "$20.000",
+  },
+
+  //Sacos
+  {
+    id: 13,
+    imagen: saco1,
+    titulo: "Saco simple",
+    descripcion: "Saco azul",
+    precio: "$20.000",
+  },
+  {
+    id: 14,
+    imagen: saco2,
+    titulo: "Saco con estampado",
+    descripcion: "Saco negro con estamapado de Puma",
+    precio: "$20.000",
+  },
+  {
+    id: 15,
+    imagen: saco3,
+    titulo: "Saco simple",
+    descripcion: "Saco gris",
+    precio: "$20.000",
+  },
+  {
+    id: 16,
+    imagen: saco4,
+    titulo: "Saco simple",
+    descripcion: "Saco rojo",
+    precio: "$20.000",
+  },
+
+  //Jeans
+  {
+    id: 17,
+    imagen: pantalon1,
+    titulo: "Jean simple",
+    descripcion: "Jean negro",
+    precio: "$20.000",
+  },
+  {
+    id: 18,
+    imagen: pantalon2,
+    titulo: "Jean simple",
+    descripcion: "Jean azul",
+    precio: "$20.000",
+  },
+  {
+    id: 19,
+    imagen: pantalon3,
+    titulo: "Jean simple",
+    descripcion: "Jean azul oscuro",
+    precio: "$20.000",
+  },
+  {
+    id: 20,
+    imagen: pantalon4,
+    titulo: "Jean simple",
+    descripcion: "Jean blanco",
+    precio: "$20.000",
+  },
+
+  //Sudadera
+  {
+    id: 21,
+    imagen: sudadera1,
+    titulo: "Sudadera simple",
+    descripcion: "Sudadera azul claro",
+    precio: "$20.000",
+  },
+  {
+    id: 22,
+    imagen: sudadera2,
+    titulo: "Sudadera simple",
+    descripcion: "Sudadera negra con lineas blancas",
+    precio: "$20.000",
+  },
+  {
+    id: 23,
+    imagen: sudadera3,
+    titulo: "Sudadera simple",
+    descripcion: "Sudadera gris con cuadros negros",
+    precio: "$20.000",
+  },
+  {
+    id: 24,
+    imagen: sudadera4,
+    titulo: "Sudadera simple",
+    descripcion: "Sudadera azul oscuro",
+    precio: "$20.000",
+  },
+
+];
