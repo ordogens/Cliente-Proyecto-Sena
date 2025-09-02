1d517935a26005f97f38d4c28c4c301ff1514d87
DiegoGuayara <diegofguayara9707@gmail.com>
2025-09-01 23:51:14 -0500

    Modificacion del componente hombre

--- src/Hombre/Hombre.tsx
+++ src/Hombre/Hombre.tsx
@@ -7,13 +7,13 @@ const opciones = [
   {
     id: 1,
     imagen: opcion1,
-    descripcion: "Camisetas, Camisas, Camibusos y Sacos",
+    descripcion: "Camisas y Busos",
     tipo: "Superior",
   },
   {
     id: 2,
     imagen: opcion2,
-    descripcion: "Jeans, Sudaderas, Joggins y Bermudas",
+    descripcion: "Pantalones y Bermudas",
     tipo: "Inferior",
   },
 ];
@@ -23,24 +23,23 @@ export const Hombre = () => {
 
   return (
     <div className="h-171 bg-[#f2f9f7]">
-      <h1 className="font-mono text-[30px] text-center">Ropa de hombre</h1>
+      <h1 className="font-bold text-[30px] text-center">Ropa de hombre</h1>
       {seleccion === "" && (
         <div className="grid grid-cols-2 mt-6 gap-4 bg-[#f2f9f7]">
           {opciones.map((img) => (
             <div key={img.id} className="flex mb-6 justify-center">
               <div
                 onClick={() => setSeleccion(img.tipo)}
-                className="flex flex-col w-60 h-[400px] bg-[#bcf7e6] pt-3.5 px-3.5 rounded-lg cursor-pointer"
+                className="flex flex-col w-60 h-[380px] shadow-2xl bg-[#f2f9f7] rounded-lg cursor-pointer"
               >
-                <div className="overflow-hidden rounded-lg shadow-md">
+                <div className="overflow-hidden rounded-lg">
                   <img
                     src={img.imagen}
                     alt={`Camisa ${img.id}`}
                     className="w-60 h-80 object-cover rounded-lg shadow-md transform duration-300 hover:scale-110"
                   />
                 </div>
-                <div className="flex justify-between"></div>
-                <p className="text-sm font-mono text-gray-900 break-words mt-2">
+                <p className="text-sm text-center font-medium text-gray-900 break-words mt-2">
                   {img.descripcion}
                 </p>
               </div>
@@ -54,19 +53,19 @@ export const Hombre = () => {
           <div className="relative grid grid-cols-4 mt-6 gap-4 bg-[#f2f9f7]">
             {parteSuperior.map((img) => (
               <div key={img.id} className="flex mb-6 justify-center">
-                <div className="flex flex-col w-60 h-[400px] bg-[#bcf7e6] pt-3.5 px-3.5 rounded-lg">
-                  <div className="overflow-hidden rounded-lg shadow-md">
+                <div className="flex flex-col w-60 h-[380px] bg-gray-400 shadow-2xl bg-v rounded-lg">
+                  <div className="overflow-hidden rounded-lg">
                     <img
                       src={img.imagen}
                       alt={`Camisa ${img.id}`}
-                      className="w-60 h-80 object-cover rounded-lg shadow-md transform duration-300 hover:scale-110"
+                      className="w-60 h-80 object-cover rounded-lg transform duration-300 hover:scale-110"
                     />
                   </div>
-                  <div className="flex justify-between">
-                    <p className="text-gray-800">{img.titulo}</p>
-                    <p className="text-green-800">{img.precio}</p>
+                  <div className="flex justify-around">
+                    <p className="font-medium text-gray-800">{img.titulo}</p>
+                    <p className="font-medium text-green-800">{img.precio}</p>
                   </div>
-                  <p className="text-sm text-gray-800 break-words">
+                  <p className="font-medium text-sm text-center text-gray-800 break-words">
                     {img.descripcion}
                   </p>
                 </div>
@@ -77,9 +76,8 @@ export const Hombre = () => {
           <div className="flex justify-center mt-6">
             <button
               onClick={() => setSeleccion("")}
-              className="px-4 py-2 w-75 bg-blue-400 cursor-pointer
-              text-white rounded-lg mb-4 transition-all 
-              duration-300 hover:bg-blue-700 ease-in-out"
+              className="px-4 py-2 w-75 shadow-2xl bg-gradient-to-r from-green-400 via-green-500 to-green-600 cursor-pointer
+              text-white rounded-lg mb-4"
             >
               Volver
             </button>
@@ -92,19 +90,19 @@ export const Hombre = () => {
           <div className="grid grid-cols-4 mt-6 gap-4 bg-[#f2f9f7]">
             {parteInferior.map((img) => (
               <div key={img.id} className="flex mb-6 justify-center">
-                <div className="flex flex-col w-60 h-[400px] bg-[#bcf7e6] pt-3.5 px-3.5 rounded-lg">
-                  <div className="overflow-hidden rounded-lg shadow-md">
+                <div className="flex flex-col w-60 h-[380px] shadow-2xl bg-gray-400 rounded-lg">
+                  <div className="overflow-hidden rounded-lg">
                     <img
                       src={img.imagen}
                       alt={`Camisa ${img.id}`}
                       className="w-60 h-80 object-cover rounded-lg shadow-md transform duration-300 hover:scale-110"
                     />
                   </div>
-                  <div className="flex justify-between">
-                    <p className="text-gray-800">{img.titulo}</p>
-                    <p className="text-green-800">{img.precio}</p>
+                  <div className="flex justify-around">
+                    <p className="font-medium text-gray-800">{img.titulo}</p>
+                    <p className="font-medium text-green-800">{img.precio}</p>
                   </div>
-                  <p className="text-sm text-gray-800 break-words">
+                  <p className="text-sm font-medium text-gray-800 text-center break-words">
                     {img.descripcion}
                   </p>
                 </div>
@@ -115,9 +113,8 @@ export const Hombre = () => {
           <div className="flex justify-center mt-6">
             <button
               onClick={() => setSeleccion("")}
-              className="px-4 py-2 w-75 bg-blue-400 cursor-pointer
-              text-white rounded-lg mb-4 transition-all 
-              duration-300 hover:bg-blue-700 ease-in-out"
+              className="px-4 py-2 shadow-2xl w-75 bg-gradient-to-r from-green-400 via-green-500 to-green-600 cursor-pointer
+              text-white rounded-lg mb-4 "
             >
               Volver
             </button>
