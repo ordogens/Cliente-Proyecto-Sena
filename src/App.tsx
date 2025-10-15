5aee6610897daa8753bfebb3fcb5dd92d852b8cc
ordogen <oarcilagarcia@gmail.com>
2025-10-15 13:50:27 -0500

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./Header";
 import { Home } from "./views/Home";
 import { Catalogo } from "./components/catalogo/Catalogo";
-import { Prendas } from "./visualizadorPrendas/Prendas";
+// import { Prendas } from "./visualizadorPrendas/Prendas";
 import { ProductsPage } from "./pages/ProductsPage";
 import { Categoria } from "./lib/helpers/Category";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -16,7 +16,7 @@ export const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/catalogo" element={<Catalogo />} />
-
+            
             <Route path="/catalogo/:sexo" element={<Categoria />} />
             <Route path="/catalogo/:sexo/:tipo" element={<ProductsPage />} />
 
