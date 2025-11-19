25a8ca3ac425e1c3f78c58c223c42e18ba76c6f1
ordogen <oarcilagarcia@gmail.com>
2025-11-18 20:28:03 -0500

    dando el primer respiro a la personalizacion

--- src/pages/Personalizacion.tsx
+++ src/pages/Personalizacion.tsx
@@ -1,12 +1,22 @@
+import { Shirt, Upload, Type, Smile, WandSparkles } from "lucide-react";
 
-
-export  const Personalizacion = () => {
+export const Personalizacion = () => {
   return (
-    <div>
-        <h1 className="text-6xl text-amber-400">SI LEE ESTO, POR FIN PUDO HPTA</h1>
-      
-    </div>
-  )
-}
+    <article className="h-[675px] bg-[#f2f9f7] flex">
+      <section className="w-[30%] h-full  border-1 border-black flex flex-col">
 
+        <section className="border-1 border-black w-full h-[3rem] flex justify-between items-center p-2">
+          <Shirt color="black"  size={30}/>
+          <Upload color="black" size={30}/>
+          <Type color="black"   size={30}/>
+          <Smile color="black"  size={30}/>
+          <WandSparkles color="black" size={30}/>
+        </section>
 
+      </section>
+      <section className="w-[70%] h-full  border-1 border-black">
+
+      </section>
+    </article>
+  );
+};
